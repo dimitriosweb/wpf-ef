c2a5de45496d62b9e92b1c9a2baa7bbf2519a8f1
dimitriosweb <dimitrios.web@outlook.com>
2018-09-07 17:37:11 +0200

    Multiple urls

--- WPF_EF/AsyncFirst/MainWindow.xaml.cs
+++ WPF_EF/AsyncFirst/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,53 +28,118 @@ namespace AsyncFirst
             InitializeComponent();
         }
 
-        // Mark the event handler with async so you can use await in it.
         private async void StartButton_Click(object sender, RoutedEventArgs e)
         {
-            // Call and await separately.
-            //Task<int> getLengthTask = AccessTheWebAsync();
-            //// You can do independent work here.
-            //int contentLength = await getLengthTask;
+            // Disable the button until the operation is complete.  
+            startButton.IsEnabled = false;
 
-            int contentLength = await AccessTheWebAsync();
+            resultsTextBox.Clear();
 
+            // One-step async call.  
+            await SumPageSizesAsync();
+
+            // Two-step async call.  
+            //Task sumTask = SumPageSizesAsync();  
+            //await sumTask;  
+
+            resultsTextBox.Text += "\r\nControl returned to startButton_Click.\r\n";
+
+            // Reenable the button in case you want to run the operation again.  
+            startButton.IsEnabled = true;
+        }
+
+        private async Task SumPageSizesAsync()
+        {
+            // Make a list of web addresses.  
+            List<string> urlList = SetUpURLList();
+
+            var total = 0;
+
+            foreach (var url in urlList)
+            {
+                byte[] urlContents = await GetURLContentsAsync(url);
+
+                // The previous line abbreviates the following two assignment statements.  
+
+                // GetURLContentsAsync returns a Task<T>. At completion, the task  
+                // produces a byte array.  
+                //Task<byte[]> getContentsTask = GetURLContentsAsync(url);  
+                //byte[] urlContents = await getContentsTask;  
+
+                DisplayResults(url, urlContents);
+
+                // Update the total.            
+                total += urlContents.Length;
+            }
+            // Display the total count for all of the websites.  
             resultsTextBox.Text +=
-                String.Format("\r\nLength of the downloaded string: {0}.\r\n", contentLength);
+                string.Format("\r\n\r\nTotal bytes returned:  {0}\r\n", total);
         }
 
-        // Three things to note in the signature:
-        //  - The method has an async modifier. 
-        //  - The return type is Task or Task<T>. (See "Return Types" section.)
-        //    Here, it is Task<int> because the return statement returns an integer.
-        //  - The method name ends in "Async."
-        async Task<int> AccessTheWebAsync()
+        private List<string> SetUpURLList()
         {
-            // You need to add a reference to System.Net.Http to declare client.
-            HttpClient client = new HttpClient();
-
-            // GetStringAsync returns a Task<string>. That means that when you await the
-            // task you'll get a string (urlContents).
-            Task<string> getStringTask = client.GetStringAsync("http://msdn.microsoft.com");
-
-            // You can do work here that doesn't rely on the string from GetStringAsync.
-            DoIndependentWork();
-
-            // The await operator suspends AccessTheWebAsync.
-            //  - AccessTheWebAsync can't continue until getStringTask is complete.
-            //  - Meanwhile, control returns to the caller of AccessTheWebAsync.
-            //  - Control resumes here when getStringTask is complete. 
-            //  - The await operator then retrieves the string result from getStringTask.
-            string urlContents = await getStringTask;
-
-            // The return statement specifies an integer result.
-            // Any methods that are awaiting AccessTheWebAsync retrieve the length value.
-            return urlContents.Length;
+            List<string> urls = new List<string>
+            {
+                "http://msdn.microsoft.com/library/windows/apps/br211380.aspx",
+                "http://msdn.microsoft.com",
+                "http://msdn.microsoft.com/library/hh290136.aspx",
+                "http://msdn.microsoft.com/library/ee256749.aspx",
+                "http://msdn.microsoft.com/library/hh290138.aspx",
+                "http://msdn.microsoft.com/library/hh290140.aspx",
+                "http://msdn.microsoft.com/library/dd470362.aspx",
+                "http://msdn.microsoft.com/library/aa578028.aspx",
+                "http://msdn.microsoft.com/library/ms404677.aspx",
+                "http://msdn.microsoft.com/library/ff730837.aspx"
+            };
+            return urls;
         }
 
-
-        void DoIndependentWork()
+        private async Task<byte[]> GetURLContentsAsync(string url)
         {
-            resultsTextBox.Text += "Working . . . . . . .\r\n";
+            // The downloaded resource ends up in the variable named content.  
+            var content = new MemoryStream();
+
+            // Initialize an HttpWebRequest for the current URL.  
+            var webReq = (HttpWebRequest)WebRequest.Create(url);
+
+            // Send the request to the Internet resource and wait for  
+            // the response.                  
+            using (WebResponse response = await webReq.GetResponseAsync())
+
+            // The previous statement abbreviates the following two statements.  
+
+            //Task<WebResponse> responseTask = webReq.GetResponseAsync();  
+            //using (WebResponse response = await responseTask)  
+            {
+                // Get the data stream that is associated with the specified url.  
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    // Read the bytes in responseStream and copy them to content.   
+                    await responseStream.CopyToAsync(content);
+
+                    // The previous statement abbreviates the following two statements.  
+
+                    // CopyToAsync returns a Task, not a Task<T>.  
+                    //Task copyTask = responseStream.CopyToAsync(content);  
+
+                    // When copyTask is completed, content contains a copy of  
+                    // responseStream.  
+                    //await copyTask;  
+                }
+            }
+            // Return the result as a byte array.  
+            return content.ToArray();
+        }
+
+        private void DisplayResults(string url, byte[] content)
+        {
+            // Display the length of each website. The string format   
+            // is designed to be used with a monospaced font, such as  
+            // Lucida Console or Global Monospace.  
+            var bytes = content.Length;
+            // Strip off the "http://".  
+            var displayURL = url.Replace("http://", "");
+            resultsTextBox.Text += string.Format("\n{0,-58} {1,8}", displayURL, bytes);
         }
     }
 }
